97529bb3c9446fef2d778170712834499d24fee4
felipereyel <felipe.reyel@gmail.com>
2022-06-17 17:19:19 -0300

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='abstra-cli',
-    version='0.0.1',
+    version='0.1.0',
     description='Abstra CLI',
     long_description=README,
     long_description_content_type="text/markdown",
