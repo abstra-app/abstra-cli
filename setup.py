58ac6ec3cb2b02ad08888f3905964678686a1fe7
felipereyel <felipe.reyel@gmail.com>
2022-06-20 11:22:54 -0300

    bump version - urllib to upload

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='abstra-cli',
-    version='0.1.1',
+    version='0.1.2',
     description='Abstra CLI',
     long_description=README,
     long_description_content_type="text/markdown",
