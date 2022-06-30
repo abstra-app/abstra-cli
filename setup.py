d581cd56eadd55b87199da8108efca5ac7cb609f
Pedro Reis <pedroreisv@gmail.com>
2022-06-30 16:38:00 -0300

    0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='abstra-cli',
-    version='0.2.0',
+    version='0.3.0',
     description='Abstra CLI',
     long_description=README,
     long_description_content_type="text/markdown",
