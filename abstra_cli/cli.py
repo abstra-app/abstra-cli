bce8aafdf09d9c4aebb6d886036a0f2b522ea17c
joaohenrifranco <joaohenrique.franco@gmail.com>
2022-10-20 11:54:12 -0300

    Run formatter

--- abstra_cli/cli.py
+++ abstra_cli/cli.py
@@ -1,7 +1,11 @@
 import fire
 
 from abstra_cli.cli_helpers import read_credentials
-from abstra_cli.utils_config import credentials_check, save_credentials, configuration_check
+from abstra_cli.utils_config import (
+    credentials_check,
+    save_credentials,
+    configuration_check,
+)
 from abstra_cli.messages import not_implemented
 
 from abstra_cli.resources import Forms
